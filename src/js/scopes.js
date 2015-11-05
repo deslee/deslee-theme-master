440849e7dbe686deae736e0188e69c20352f3a9d
Desmond Lee <desmondclee@gmail.com>
2015-11-04 22:50:38 -0600

    theme updates

--- src/js/scopes.js
+++ src/js/scopes.js
@@ -11,6 +11,9 @@ var scope = {
             name: "About"
         },
         {
+            name: "Archive"
+        },
+        {
             name: "Projects"
         },
         
@@ -20,22 +23,26 @@ var scope = {
         {
             title: 'Getting started with Google Calendar API',
             date: '2014-11-23',
-            tags: ['apis', 'dev', 'test']
+            tags: ['apis', 'dev', 'test'],
+            preview: 'I have begun experimenting with Google Apps Application APIs, with the intention of building a Javascript client-side application to interface with Google Calendar. »'
         },
         {
             title: 'Static blog with React and Flux',
             date: '2014-11-23',
-            tags: ['apis', 'dev', 'test']
+            tags: ['apis', 'dev', 'test'],
+            preview: 'I have begun experimenting with Google Apps Application APIs, with the intention of building a Javascript client-side application to interface with Google Calendar. I\'m going to demonstrate what I\'ve learned with a scenario flow for accessing the Google Calendar REST API »'
         },
         {
             title: 'Experimented with Polymer',
             date: '2014-11-23',
-            tags: ['apis', 'dev', 'test']
+            tags: ['apis', 'dev', 'test'],
+            preview: 'I have begun experimenting with Google Apps Application APIs »'
         },
         {
             title: 'Learning how to use ReactJS and Firebase',
             date: '2014-11-23',
-            tags: ['apis', 'dev', 'test']
+            tags: ['apis', 'dev', 'test'],
+            preview: 'I have begun experimenting with Google Apps Application APIs, with the intention of building a Javascript client-side »'
         }
     ]
 };
